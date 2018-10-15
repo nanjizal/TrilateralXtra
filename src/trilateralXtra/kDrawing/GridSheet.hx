8d49027c576605c38c6231579e78dcac8664bc58
nanjizal <nanjizal@users.noreply.github.com>
2018-10-15 19:55:25 +0100

    added depth

--- src/trilateralXtra/kDrawing/GridSheet.hx
+++ src/trilateralXtra/kDrawing/GridSheet.hx
@@ -13,11 +13,17 @@ typedef GridSheetDef = {
     var image:      Image;
 }
 typedef GridItemDef = {
-    var x: Float;
-    var y: Float;
-    var alpha: Float;
-    var color: Color;
+    var x:      Float;
+    var y:      Float;
+    var alpha:  Float;
+    var color:  Color;
     var transform: FastMatrix3;
+    var depth:  Int;
+}
+typedef ItemAtt = {
+    var item: GridItemDef;
+    var col: Int;
+    var row: Int;
 }
 typedef GridItems = {
     function getItem( row: Float, col: Float ): GridItemDef;
@@ -57,20 +63,63 @@ class GridSheet{
     public function renderGrid( g: Graphics, gridItems: GridItems, ?outline: Bool = false ){
         var p: { x: Float, y: Float };
         var alpha: Float;
-        for( col in 0...totalCols ){
-            for( row in 0...totalRows ){
-                renderFrame( g, gridItems, col, row, outline );
+        var item: GridItemDef;
+        var temp: Array<ItemAtt> = [];
+        var count = 0;
+        for( row in 0...totalRows ){
+            for( col in 0...totalCols ){
+                item = gridItems.getItem( col, row );
+                temp[ count++ ] = { item: item, col: col, row: row };
             }
         }
-        // assume to reset it to 1.
-        g.transformation = FastMatrix3.identity();
+        depthSort( temp );
+        var obj: ItemAtt;
+        for( i in 0...temp.length ){
+            obj = temp[ i ];
+            renderItem( g, obj.item, obj.col, obj.row, outline );
+        }
+        resetGraphics( g );
+    }
+    inline function resetGraphics( g: Graphics ){
+        g.color = Color.White;
+        g.transformation = FastMatrix3.identity(); 
         g.opacity = 1.;
     }
+    inline function depthSort( arr: Array<ItemAtt> ){
+        haxe.ds.ArraySort.sort( arr, function( a, b ): Int {
+            var c = a.item.depth;
+            var d = b.item.depth;
+            if( c < d ){
+                return -1;
+            } else if ( c > d ){ 
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+    } 
     public function renderSequence( g: Graphics, gridItems: GridItems ){
         renderFrame( g, gridItems, r, c );
         advanceFrame();
     }
-    inline function renderFrame( g: Graphics, gridItems: GridItems, row: Int, col: Int, ?outline: Bool = false ){
+    inline function renderItem( g: Graphics, item: GridItemDef, col: Int, row: Int, ?outline: Bool = false ){
+        g.opacity = item.alpha;
+        g.transformation = item.transform;
+        if( outline ){
+            g.color = Color.Red;
+            g.drawRect( item.x, item.y, gridX, gridY, 1 );
+            g.color = item.color;
+        }
+        g.color = item.color;
+        g.drawScaledSubImage( image
+                            , col*gridX + dx, row*gridY + dy
+                            , gridX, gridY
+                            , item.x, item.y
+                            , gridX * scaleX, gridY * scaleY );
+        g.color = Color.White;
+    }
+    
+    inline function renderFrame( g: Graphics, gridItems: GridItems, col: Int, row: Int, ?outline: Bool = false ){
         var item = gridItems.getItem( row, col );
         g.opacity = item.alpha;
         g.transformation = item.transform;
@@ -81,7 +130,7 @@ class GridSheet{
         }
         g.color = item.color;
         g.drawScaledSubImage( image
-                            , row*gridX + dx, col*gridY + dy
+                            , col*gridX + dx, row*gridY + dy
                             , gridX, gridY
                             , item.x, item.y
                             , gridX * scaleX, gridY * scaleY );
@@ -102,8 +151,14 @@ class GridSheet{
         count++;
     }
     // default one to give a grid layout.
-    inline function getItem( row: Int, col: Int ): GridItemDef {
-        return { x: scaleX*row*gridX, y: scaleY*col*gridY, alpha: 1., color: Color.White, transform: FastMatrix3.identity() };
+    inline function getItem( col: Int, row: Int ): GridItemDef {
+        return { x:         scaleX*col*gridX
+               , y:         scaleY*row*gridY
+               , color:     Color.White
+               , alpha:     1.
+               , transform: FastMatrix3.identity()
+               , depth: 0
+           };
     }
 }
 class SequenceSprite{
@@ -112,14 +167,16 @@ class SequenceSprite{
     public var color: Color = Color.White;
     public var alpha: Float = 0;
     public var matrix: FastMatrix3;
-    public function new( x_: Float, y_: Float, color_: Color, alpha_: Float, matrix_: FastMatrix3 ){
+    public var depth: Int = 0;
+    public function new( x_: Float, y_: Float, color_: Color, alpha_: Float, matrix_: FastMatrix3, depth_: Int ){
         x = x_;
         y = y_;
         color = color_;
         alpha = alpha_;
         matrix = matrix_;
+        depth = depth_;
     }
-    inline function getItem( row: Int, col: Int ): GridItemDef {
-        return { x: x, y: y, color: color, alpha: alpha, transform: matrix };
+    inline function getItem( col: Int, row: Int ): GridItemDef {
+        return { x: x, y: y, color: color, alpha: alpha, transform: matrix, depth: depth };
     }
 }
