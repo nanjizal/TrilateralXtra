899467bc850cfe4dd087034a3c2b2ab151c7b683
nanjizal <nanjizal@users.noreply.github.com>
2019-01-03 05:36:52 +0000

    factor some into FillDraw and add contour, polyk still needs adjusting

--- src/trilateralXtra/parsing/FillDraw2Tri.hx
+++ src/trilateralXtra/parsing/FillDraw2Tri.hx
@@ -11,9 +11,8 @@ class FillDraw2Tri extends FillDraw {
     function new( ?w: Int, ?h: Int ){
         super( w, h );
     }
-    // utilize base.pointsNoEndOverlap() to get clean points!
-    override public
-    function fill( points: Array<Array<Float>>, colorID: Int ){
+    override
+    function fillFunc( p: Array<Array<Float>> ):TfillDatas {
         var vp = new VisiblePolygon();
         var l = points.length;
         var p: Array<Float>;
@@ -36,51 +35,7 @@ class FillDraw2Tri extends FillDraw {
         var pt = vp.getVerticesAndTriangles();
         var tri = pt.triangles;
         var vert = pt.vertices;
-        var triples = new ArrayTriple( tri );
-        var i: Int;
-        for( tri in triples ){
-            var a: Int = Std.int( tri.a*3 );
-            var b: Int = Std.int( tri.b*3 );
-            var c: Int = Std.int( tri.c*3 );
-            triangles.drawTriangle(  count, { x: vert[ a ], y: vert[ a + 1 ] }
-                                       , { x: vert[ b ], y: vert[ b + 1 ] }
-                                       , { x: vert[ c ], y: vert[ c + 1 ] }, colorID );
-        }
-    }
-    override public
-    function fillRnd( points: Array<Array<Float>>, rnd: Int ){
-        var vp = new VisiblePolygon();
-        var l = points.length;
-        var p: Array<Float>;
-        for( i in 0...l ){
-            p = points[ i ];
-            if( p.length != 0 ) {
-                var p2t: Array<org.poly2tri.Point> = [];
-                var pairs = new ArrayPairs<Float>( p );
-                var p0 = pairs[0].x;
-                var p1 = pairs[0].y;
-                for( pair in pairs ) p2t.push( new org.poly2tri.Point( pair.x, pair.y ) );
-                var l2 = p2t.length;
-                if( p0 == p2t[ l2 - 1 ].x && p1 == p2t[ l2 - 1 ].y ){
-                    p2t.pop();
-                }
-                vp.addPolyline( p2t );
-            }
-        }
-        vp.performTriangulationOnce();
-        var pt = vp.getVerticesAndTriangles();
-        var tri = pt.triangles;
-        var vert = pt.vertices;
-        var triples = new ArrayTriple( tri );
-        var i: Int;
-        for( tri in triples ){
-            var a: Int = Std.int( tri.a*3 );
-            var b: Int = Std.int( tri.b*3 );
-            var c: Int = Std.int( tri.c*3 );
-            triangles.drawTriangle(  count, { x: vert[ a ], y: vert[ a + 1 ] }
-                                       , { x: vert[ b ], y: vert[ b + 1 ] }
-                                       , { x: vert[ c ], y: vert[ c + 1 ] }, Std.int( Math.round( Math.random()*rnd ) ) );//colorID );
-        }
+        return { vert: vert, tri: tri }
     }
     override public
     function pathFactory(): Base {
